c7db3ee9bfbc223f24bfaa83fd9bd6d727f2f360
Huseyin <huseyintkt41@gmail.com>
2022-02-24 21:16:05 +0100

    Project is updated

--- src/ch/hicoders/Application.java
+++ src/ch/hicoders/Application.java
@@ -1,6 +1,7 @@
 package ch.hicoders;
 
 import ch.hicoders.classroom.Classroom;
+import ch.hicoders.person.Person;
 import ch.hicoders.student.Student;
 import ch.hicoders.teacher.Specialty;
 import ch.hicoders.teacher.Teacher;
@@ -11,22 +12,22 @@ public class Application {
 
     public static void main(String[] args) {
 
-        Student student1 = new Student("Ahmet","Memis", LocalDate.parse("2000-12-01"));
-        Student student2 = new Student("Okkes","Memis", LocalDate.parse("2000-11-01"));
-        Student student3 = new Student("Ayse","Memis", LocalDate.parse("2000-12-11"));
-        Student student4 = new Student("Ali","Memis", LocalDate.parse("2000-12-12"));
-        Student student5 = new Student("Veli","Memis", LocalDate.parse("2000-12-13"));
-        Student student6 = new Student("Mehmet","Memis", LocalDate.parse("2000-12-14"));
-        Student student7 = new Student("Huseyin","Memis", LocalDate.parse("2000-12-15"));
-        Student student8 = new Student("Osman","Memis", LocalDate.parse("2000-12-16"));
-        Student student9 = new Student("Bekir","Memis", LocalDate.parse("2000-12-17"));
-        Student student10 = new Student("Fatih","Memis", LocalDate.parse("2000-12-18"));
+        Person student1 = new Student("Ahmet","Memis", LocalDate.parse("2000-12-01"));
+        Person student2 = new Student("Okkes","Memis", LocalDate.parse("2000-11-01"));
+        Person student3 = new Student("Ayse","Memis", LocalDate.parse("2000-12-11"));
+        Person student4 = new Student("Ali","Memis", LocalDate.parse("2000-12-12"));
+        Person student5 = new Student("Veli","Memis", LocalDate.parse("2000-12-13"));
+        Person student6 = new Student("Mehmet","Memis", LocalDate.parse("2000-12-14"));
+        Person student7 = new Student("Huseyin","Memis", LocalDate.parse("2000-12-15"));
+        Person student8 = new Student("Osman","Memis", LocalDate.parse("2000-12-16"));
+        Person student9 = new Student("Bekir","Memis", LocalDate.parse("2000-12-17"));
+        Person student10 = new Student("Fatih","Memis", LocalDate.parse("2000-12-18"));
 
-        Student[] studentGroup1 = {student1, student2, student3, student4};
-        Student[] studentGroup2= {student5, student6, student7, student8, student9, student10};
+        Person[] studentGroup1 = {student1, student2, student3, student4};
+        Person[] studentGroup2= {student5, student6, student7, student8, student9, student10};
 
-        Teacher teacher1 = new Teacher("Cevdet","Demir", LocalDate.parse("1988-12-01"), Specialty.MATH);
-        Teacher teacher2 = new Teacher("Aslı","Demir", LocalDate.parse("1990-12-01"), Specialty.ENGLISH);
+        Person teacher1 = new Teacher("Cevdet","Demir", LocalDate.parse("1988-12-01"), Specialty.MATH);
+        Person teacher2 = new Teacher("Aslı","Demir", LocalDate.parse("1990-12-01"), Specialty.ENGLISH);
 
         Classroom classroom1 = new Classroom(teacher1,studentGroup1,"Zurih","Class A");
         Classroom classroom2 = new Classroom(teacher2,studentGroup2,"Luzern","Class B");
--- src/ch/hicoders/classroom/Classroom.java
+++ src/ch/hicoders/classroom/Classroom.java
@@ -1,26 +1,27 @@
 package ch.hicoders.classroom;
 
+import ch.hicoders.person.Person;
 import ch.hicoders.student.Student;
 import ch.hicoders.teacher.Teacher;
 
 public class Classroom {
-    private Teacher teacher;
-    private Student[] students;
+    private Person teacher;
+    private Person[] students;
     private String location;
     private String name;
 
-    public Classroom(Teacher pTeacher, Student[] pStudents, String pLocation, String pName){
+    public Classroom(Person pTeacher, Person[] pStudents, String pLocation, String pName){
         this.setTeacher(pTeacher);
         this.setStudents(pStudents);
         this.setLocation(pLocation);
         this.setName(pName);
     }
 
-    public void setTeacher(Teacher teacher) {
+    public void setTeacher(Person teacher) {
         this.teacher = teacher;
     }
 
-    public void setStudents(Student[] students) {
+    public void setStudents(Person[] students) {
         this.students = students;
     }
 
